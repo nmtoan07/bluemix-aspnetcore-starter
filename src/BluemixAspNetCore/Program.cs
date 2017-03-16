57983480759eee936431cac4338f869da798b090
Toan Nguyen <nmtoan07@hotmail.com>
2017-03-16 23:08:38 +0700

    Init project source

---
+++ src/BluemixAspNetCore/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+
+namespace BluemixAspNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
